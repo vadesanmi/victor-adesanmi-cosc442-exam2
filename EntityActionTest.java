8056497c381278a3fef2435637edf2f8f1da7a5b
vadesanmi <victor.o.adesanmi@gmail.com>
2016-05-16 16:58:46 -0400

    Add files via upload
    Test class added for Entity ACtion class

---
+++ EntityActionTest.java
@@ -0,0 +1,52 @@
+package megamek.common.actions;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * The class <code>EntityActionTest</code> contains tests for the class <code>{@link EntityAction}</code>.
+ *
+ * @generatedBy CodePro at 5/16/16 4:19 PM
+ * @author vadesa1
+ * @version $Revision: 1.0 $
+ */
+public class EntityActionTest {
+	/**
+	 * Perform pre-test initialization.
+	 *
+	 * @throws Exception
+	 *         if the initialization fails for some reason
+	 *
+	 * @generatedBy CodePro at 5/16/16 4:19 PM
+	 */
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 *
+	 * @throws Exception
+	 *         if the clean-up fails for some reason
+	 *
+	 * @generatedBy CodePro at 5/16/16 4:19 PM
+	 */
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 5/16/16 4:19 PM
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(EntityActionTest.class);
+	}
+}
